bf1920cbc4d056066f43363bd9b2a7e26b470091
Ibrohim1702 <88259165+Ibrohim1702@users.noreply.github.com>
2023-02-27 18:23:50 +0500

    Update settings.py

--- Mebel/settings.py
+++ Mebel/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static/root")
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
